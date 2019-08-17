fa5c6837c5b33fb1f6b9bd3b827ad9eb371835e0
adenchua <37574101+adenchua@users.noreply.github.com>
2019-08-17 22:06:39 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Typography>Hello This is in roboto fonts</Typography>
+    </div>
+  );
+};
+
+export default App;
